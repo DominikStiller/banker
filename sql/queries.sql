e96f6a3eb43a7bede0270fb65f57d32c0af1bea4
Dominik Stiller <dominik.stiller@hpe.com>
2019-04-11 11:11:03 -0700

    Add all queries to sql file

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,6 +1,18 @@
 -- -----------------------------------------------------------------------------------
--- Some of the more complicated queries which are used in the Java application
+-- All queries which are used in the Java application
 -- -----------------------------------------------------------------------------------
+
+
+-- Get a specific customer and number of associated accounts
+SELECT id, name, address, email, phone, COUNT(account_no)
+FROM customers
+LEFT JOIN accounts ON customers.id = accounts.customer_id
+WHERE id = ?
+GROUP BY id, name, address, email, phone;
+
+
+-- -----------------------------------------------------------------------------------
+
 
 -- Get all customers and numbers of associated accounts
 SELECT id, name, address, email, phone, COUNT(account_no)
@@ -12,15 +24,31 @@ GROUP BY id, name, address, email, phone;
 -- -----------------------------------------------------------------------------------
 
 
--- Get customer and number of associated accounts
-SELECT id, name, address, email, phone, COUNT(account_no)
-FROM customers
-LEFT JOIN accounts ON customers.id = accounts.customer_id
-WHERE id = ?
-GROUP BY id, name, address, email, phone;
+-- Create a new customer
+INSERT INTO customers (name, address, email, phone)
+VALUES (?, ?, ?, ?);
 
 
 -- -----------------------------------------------------------------------------------
+
+
+-- Get a specific account
+SELECT *
+FROM accounts
+WHERE customer_id = ?
+AND account_no = ?;
+
+
+-- -----------------------------------------------------------------------------------
+
+
+-- Get all accounts for a specific customer
+SELECT * FROM accounts
+WHERE customer_id = ?;
+
+
+-- -----------------------------------------------------------------------------------
+
 
 -- Get account balance
 -- balance = initial_balance + incoming - outgoing
@@ -65,7 +93,15 @@ AND account_no = ?;
 -- -----------------------------------------------------------------------------------
 
 
--- Get transfers
+-- Create a new account
+INSERT INTO accounts (customer_id, account_no, currency, initial_balance)
+VALUES (?, ?, ?, ?);
+
+
+-- -----------------------------------------------------------------------------------
+
+
+-- Get incoming and outgoing transfers for a specific account
 SELECT transfers.id,
        sender_id, sender_account, s.name as sender_name,
        receiver_id, receiver_account, r.name as receiver_name,
@@ -73,7 +109,9 @@ SELECT transfers.id,
 FROM transfers
 JOIN customers s ON transfers.sender_id = s.id
 JOIN customers r ON transfers.receiver_id = r.id
+-- Outgoing
 WHERE (sender_id = ? AND sender_account = ?)
+-- Incoming
 OR (receiver_id = ? AND receiver_account = ?)
 ORDER BY execution_date ASC;
 
@@ -81,7 +119,23 @@ ORDER BY execution_date ASC;
 -- -----------------------------------------------------------------------------------
 
 
--- Convert currency
+-- Make wire transfer
+INSERT INTO transfers(sender_id, sender_account, receiver_id, receiver_account,
+                  amount, currency, execution_date, reference)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+
+
+-- -----------------------------------------------------------------------------------
+
+
+-- Get all currencies
+SELECT code, name FROM currencies;
+
+
+-- -----------------------------------------------------------------------------------
+
+
+-- Convert amount of money between currencies
 SELECT ? * rate
 FROM exchangerates
 WHERE from_currency = ?
